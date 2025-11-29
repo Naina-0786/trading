c6be86ed2a34d9d949e7ada8a895245b076f589a
Ssaif3 <samsaif933@gmail.com>
2025-11-29 12:50:56 +0530

    jjahdad

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const app = express();
 
 app.use(cors({
   // origin: "*",
-  origin: ["http://localhost:8080", "http://localhost:8081"],
+  origin: ["http://localhost:8080", "http://localhost:8081", "https://www.expotradex.com","https://expo-trading-admin.vercel.app/"],
   credentials: true,
 }));
 
