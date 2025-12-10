fa0ad98ff34173f60507775fc6c648d6fcc42ba1
amitkumardss012 <amitkumaryadav62029@gmail.com>
2025-12-10 19:34:09 +0530

    changed refurl

--- src/controller/userDashboard.ts
+++ src/controller/userDashboard.ts
@@ -143,7 +143,7 @@ export const getUserDashboard = asyncHandler(async (req, res, next) => {
   });
 
   // Referral Link
-  const referralLink = `https://www.expotradex.com/?ref=${user.referralCode}`;
+  const referralLink = `https://www.expotradex.com/auth/signup/?ref=${user.referralCode}`;
 
   // Structured Dashboard Data
   const dashboardData = {
